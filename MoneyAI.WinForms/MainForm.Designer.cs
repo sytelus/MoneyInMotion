899cb4f5f8ecae64446bfee54c2a7d24b5689e7d
Shital Shah <shital@ShitalShah.com>
2013-09-03 01:55:05 -0700

    Updated WinForms layout

--- MoneyAI.WinForms/MainForm.Designer.cs
+++ MoneyAI.WinForms/MainForm.Designer.cs
@@ -31,16 +31,26 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBoxRootFolder = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonSaveLatestMerged = new System.Windows.Forms.Button();
+            this.buttonScanStatements = new System.Windows.Forms.Button();
             this.buttonAddAccount = new System.Windows.Forms.Button();
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
-            this.buttonScanStatements = new System.Windows.Forms.Button();
-            this.buttonSaveLatestMerged = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusStrip2 = new System.Windows.Forms.StatusStrip();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -76,6 +86,26 @@
             this.panel1.Size = new System.Drawing.Size(1393, 48);
             this.panel1.TabIndex = 2;
             // 
+            // buttonSaveLatestMerged
+            // 
+            this.buttonSaveLatestMerged.Location = new System.Drawing.Point(735, 11);
+            this.buttonSaveLatestMerged.Name = "buttonSaveLatestMerged";
+            this.buttonSaveLatestMerged.Size = new System.Drawing.Size(168, 23);
+            this.buttonSaveLatestMerged.TabIndex = 7;
+            this.buttonSaveLatestMerged.Text = "Save Latest Merged";
+            this.buttonSaveLatestMerged.UseVisualStyleBackColor = true;
+            this.buttonSaveLatestMerged.Click += new System.EventHandler(this.buttonSaveLatestMerged_Click);
+            // 
+            // buttonScanStatements
+            // 
+            this.buttonScanStatements.Location = new System.Drawing.Point(554, 11);
+            this.buttonScanStatements.Name = "buttonScanStatements";
+            this.buttonScanStatements.Size = new System.Drawing.Size(165, 23);
+            this.buttonScanStatements.TabIndex = 6;
+            this.buttonScanStatements.Text = "Scan Statements";
+            this.buttonScanStatements.UseVisualStyleBackColor = true;
+            this.buttonScanStatements.Click += new System.EventHandler(this.buttonScanStatements_Click);
+            // 
             // buttonAddAccount
             // 
             this.buttonAddAccount.Location = new System.Drawing.Point(1159, 10);
@@ -104,11 +134,15 @@
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.richTextBoxLog);
             this.splitContainer1.Size = new System.Drawing.Size(1389, 703);
-            this.splitContainer1.SplitterDistance = 348;
+            this.splitContainer1.SplitterDistance = 588;
             this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 4;
             // 
@@ -118,29 +152,61 @@
             this.richTextBoxLog.Location = new System.Drawing.Point(0, 0);
             this.richTextBoxLog.Margin = new System.Windows.Forms.Padding(4);
             this.richTextBoxLog.Name = "richTextBoxLog";
-            this.richTextBoxLog.Size = new System.Drawing.Size(1387, 348);
+            this.richTextBoxLog.Size = new System.Drawing.Size(1387, 108);
             this.richTextBoxLog.TabIndex = 0;
             this.richTextBoxLog.Text = "";
             // 
-            // buttonScanStatements
+            // splitContainer2
             // 
-            this.buttonScanStatements.Location = new System.Drawing.Point(554, 11);
-            this.buttonScanStatements.Name = "buttonScanStatements";
-            this.buttonScanStatements.Size = new System.Drawing.Size(165, 23);
-            this.buttonScanStatements.TabIndex = 6;
-            this.buttonScanStatements.Text = "Scan Statements";
-            this.buttonScanStatements.UseVisualStyleBackColor = true;
-            this.buttonScanStatements.Click += new System.EventHandler(this.buttonScanStatements_Click);
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
             // 
-            // buttonSaveLatestMerged
+            // splitContainer2.Panel1
             // 
-            this.buttonSaveLatestMerged.Location = new System.Drawing.Point(735, 11);
-            this.buttonSaveLatestMerged.Name = "buttonSaveLatestMerged";
-            this.buttonSaveLatestMerged.Size = new System.Drawing.Size(168, 23);
-            this.buttonSaveLatestMerged.TabIndex = 7;
-            this.buttonSaveLatestMerged.Text = "Save Latest Merged";
-            this.buttonSaveLatestMerged.UseVisualStyleBackColor = true;
-            this.buttonSaveLatestMerged.Click += new System.EventHandler(this.buttonSaveLatestMerged_Click);
+            this.splitContainer2.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer2.Panel1.Controls.Add(this.statusStrip1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.listView1);
+            this.splitContainer2.Panel2.Controls.Add(this.statusStrip2);
+            this.splitContainer2.Size = new System.Drawing.Size(1387, 586);
+            this.splitContainer2.SplitterDistance = 462;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 564);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(462, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusStrip2
+            // 
+            this.statusStrip2.Location = new System.Drawing.Point(0, 564);
+            this.statusStrip2.Name = "statusStrip2";
+            this.statusStrip2.Size = new System.Drawing.Size(921, 22);
+            this.statusStrip2.TabIndex = 0;
+            this.statusStrip2.Text = "statusStrip2";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(462, 564);
+            this.treeView1.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(921, 564);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // FormMain
             // 
@@ -152,14 +218,22 @@
             this.Controls.Add(this.panel1);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FormMain";
-            this.Text = "MiM";
+            this.Text = "MoneyAI - Smart Personal Finance in 15 Minutes";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormMain_FormClosed);
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -175,6 +249,11 @@
         private System.Windows.Forms.Button buttonAddAccount;
         private System.Windows.Forms.Button buttonScanStatements;
         private System.Windows.Forms.Button buttonSaveLatestMerged;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.StatusStrip statusStrip2;
     }
 }
 
