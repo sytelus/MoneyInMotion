92454996d536cb062e5e680b85d518ebd6cb4853
Shital Shah <sytelus@gmail.com>
2013-10-30 20:19:51 -0700

    JS client setup

---
+++ MoneyAI.JS/js/app.js
@@ -0,0 +1,22 @@
+﻿requirejs.config({
+    //To get timely, correct error triggers in IE, force a define/shim exports check.
+    //enforceDefine: true,
+    baseUrl: "js",
+    paths: {
+        jquery: ['ext/jquery-1.10.2', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'],  //fallback 
+        jqueryui: ['ext/jquery-ui-1.10.3/ui/jquery-ui', '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js'],
+        domReady: 'ext/domReady'
+    },
+    shim: {
+        jqueryui: ['jquery']
+    }
+});
+
+
+require(['domReady', 'repository', 'jquery'], function (domReady, repository, $) {
+    domReady(function () {
+        repository.getTransactions(function (data) {
+            $("#log").text(data.Name);
+        });
+    });
+});
---
+++ MoneyAI.JS/js/repository.js
@@ -0,0 +1,17 @@
+﻿define(['jquery'], function ($) {
+    return {
+        getTransactions: function (onGet, onFail) {
+            var jqxhr = $.getJSON("data/LatestMerged.json", function (data, textStatus) {
+                console.log(textStatus);
+                onGet(data);
+            })
+          .fail(function (jqxhr, textStatus, error) {
+              !!onFail && onFail(error);
+              console.log(textStatus);
+          })
+          .always(function () {
+              console.log("getTransactions complete");
+          });
+        }
+    };
+});
